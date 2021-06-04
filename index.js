869c0655560a3eede009bac9daab0420d9dfd15a
botchway44 <noelnuel44@gmail.com>
2021-06-04 10:24:42 +0000

    cleanups

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Import the appropriate service and chosen wrappers
 const {
-  conversation,
-  Image,
-} = require('@assistant/conversation')
+    conversation,
+    Image,
+} = require('@assistant/conversation');
 
 // Create an app instance
-const app = conversation()
+const app = conversation();
 
 // Register handlers for Actions SDK
 
-app.handle('<YOUR HANDLER NAME>', conv => {
-  conv.add('Hi, how is it going?')
-  conv.add(new Image({
-    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-    alt: 'A cat',
-  }))
-})
+app.handle('AddTaskIntent', conv => {
+    conv.add('Hi, how is it going?');
+    conv.add(new Image({
+        url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+        alt: 'A cat',
+    }));
+});
